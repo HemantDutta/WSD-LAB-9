216a9a379c955bb6e4bf671c63210a2b64baca15
HemantDutta <hemantnorddutta2@gmail.com>
2022-12-27 01:08:04 +0530

    index file initialization

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.listen(port, ()=>{
     console.log('Server is listening on port '+port);
 });
 
+app.use(express.static(__dirname + "/"));
+
 app.get("/", (req,res)=>{
    res.sendFile(__dirname + "/index.html");
 });
