283917970a5470c610a48ec69c9efd1c3f296b66
HemantDutta <hemantnorddutta2@gmail.com>
2022-12-27 00:18:26 +0530

    server creation and nodemon

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+const port = 3000;
+
+app.listen(port, ()=>{
+    console.log('Server is listening on port '+port);
+});
+
+app.get("/", (req,res)=>{
+   res.sendFile(__dirname + "/index.html");
+});
+
