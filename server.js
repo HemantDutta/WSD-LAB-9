5f5d49b6f0726d8d24dbff0e007bfa9fcb92edbc
HemantDutta <hemantnorddutta2@gmail.com>
2022-12-28 19:33:24 +0530

    file reading node js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
+
+//To grab information that is posted, we need to use the following statement
+app.use(bodyParser.urlencoded({extended: true}));
 
 const port = 3000;
 
@@ -16,6 +20,14 @@ app.get("/", (req,res)=>{
 
 app.get("/read", (req,res)=>{
    res.sendFile(__dirname + "/read.html");
+});
 
+app.post("/readFile", (req,res)=>{
+    //Body parser is needed for using req.body
+    let fileName = req.body.fileName;
+
+    //Reading file
+    let data = JSON.parse(fs.readFileSync(fileName+".json", 'utf-8'));
+    res.send(data);
 });
 
